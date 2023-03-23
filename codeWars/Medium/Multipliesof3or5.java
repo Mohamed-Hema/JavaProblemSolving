c1eaf5c65e485b2047931b91c23ca7144014bcad
Mohamed <mohamedhema2017@gmail.com>
2023-03-23 07:37:44 +0200

    leetCode

---
+++ codeWars/Medium/Multipliesof3or5.java
@@ -0,0 +1,21 @@
+package codeWars.Medium;
+
+public class Multipliesof3or5 {
+    public class Solution {
+
+        public int solution(int number) {
+            if (number < 0) {
+                return 0;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < number; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
